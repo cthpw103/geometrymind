42998c29bb5d6154e9792113441c6b3a8daa7ac0
prometheus :) <44480063+prometheus1337@users.noreply.github.com>
2019-05-04 19:18:08 +0100

    r

--- gm.py
+++ gm.py
@@ -2,7 +2,7 @@ import mindwave
 import pynput
 hd = mindwave.headset('/dev/tty.MindWave', '625f')
 keyr = pynput.controller()
-print "geometrymind by prometheus_v! |need help? discord.gg/e2WYCuk join my discord!"
+print "geometrymind by prometheus_v! | need help? discord.gg/e2WYCuk join my discord!"
 hd.connect()
 print "connecting.... boop boop boop"
 while hd.status  != "connected"
