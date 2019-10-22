0c44e4ed1c36f1141edfd8795464794ef028460c
cth103 <cthdev@email.com>
2019-10-22 22:15:22 +0100

    link + py3

--- gm.py
+++ gm.py
@@ -2,19 +2,21 @@ import mindwave
 import pynput
 hd = mindwave.headset('/dev/tty.MindWave', '625f')
 keyr = pynput.controller()
-print "geometrymind by prometheus_v! | need help? discord.gg/e2WYCuk join my discord!"
+print("geometrymind by prometheus_v! | need help? discord.gg/e2WYCuk join my discord!")
 hd.connect()
-print "connecting.... boop boop boop"
-while hd.status  != "connected"
-print "...? where is your mindwave headset"
-if hd.status == "standby"
-hd.connect();
-print "tryna connect"
-print "boom found"
+print("connecting.... boop boop boop")
 
+while hd.status  != "connected":
+  print("...? where is your mindwave headset")
+  
+if hd.status == "standby":
+  hd.connect()
+  print("tryna connect")
+  print("boom found")
+
+def gotchu(hd, str):
+    print("blink?")
+    keyr.press(key.space);
 
 while True:
-def gotchu(hd, str):
-print "blink?"
-keyr.press(key.space);
-hd.blink_handlers.append(gotchu);
+  hd.blink_handlers.append(gotchu);
